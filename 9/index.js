5883b3be0c08383049fb2cbfb4c776237929b19f
Rime <madmanspoiler277@gmail.com>
2023-08-16 18:15:24 +0300

    9 bug fix

--- 9/index.js
+++ 9/index.js
@@ -3,9 +3,7 @@
 let num = +prompt('Enter the number from 2 to 9');
 
 if (num >= 2 && num <= 9) {
-    for (let i = 0; i < 10; i++) {
-        if (i >= 1) {
-            console.log(`${num} * ${i} = ` + 2 * i)
-        }
+    for (let i = 1; i < 11; i++) {
+            console.log(`${num} * ${i} = ` + num * i)
     }
 }
